ee8f25fbf45140c570e31a11c439f43cc96b554d
bvanrooy <bart.van.rooy@gmail.com>
2020-09-30 10:48:02 +0200

    exercise A2

---
+++ JavaTest/ExA2/src/test/java/TestPerson.java
@@ -0,0 +1,19 @@
+import be.abis.exa2.model.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPerson {
+
+    @Test
+    public void ageOfPersonShouldBe42(){
+        Person person = new Person(2,"John","Doe", LocalDate.of(1978, 8, 10));
+
+        int age = person.calculateAge();
+
+        assertEquals(42,age);
+    }
+}
