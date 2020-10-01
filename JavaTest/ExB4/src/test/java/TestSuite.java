b3e2e30feddf087d476ec16277e1843758d42eb6
bvanrooy <bart.van.rooy@gmail.com>
2020-10-01 10:36:15 +0200

    exercise B4

---
+++ JavaTest/ExB4/src/test/java/TestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        TestAddress.class,
+        TestPerson.class
+})
+
+public class TestSuite {
+
+}
